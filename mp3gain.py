c8f9dbcebf7e322182d1d2305187e3fbff51b397
dtsteinm <dtsteinm@gmail.com>
2013-03-15 12:34:17 -0400

    Initial commit

---
+++ mp3gain.py
@@ -0,0 +1,219 @@
+#!/usr/bin/env python
+
+import os
+import subprocess
+import re
+
+# mp3gain Python module
+# Written by Dylan Steinmetz <dtsteinm@mtu.edu>
+# Last updated: March 5, 2013
+
+""" Recursively tag MP3 files with ReplayGain attributes """ \
+        """using the mp3gain utility."""
+
+
+# walk looks for directories containing mp3 files, 
+# and calls mp3gain() when we have something to do.
+# TODO: Add messages displaying progress of entire file structure.
+def walk(start_dir):
+    """Traverses the filesystem structure, looking for """ \
+            """directories containing MP3 files, and """ \
+            """calls mp3gain() when appropriate.
+
+    Example: mp3gain.walk('/path/to/Music')
+    """
+
+    # Clean up supplied directory (whitespace, trailing /).
+    start_dir = start_dir.strip()
+    if start_dir[-1] is os.sep:
+        start_dir = start_dir[:-1]
+
+    # Since this can run for a while, let's make a private
+    # copy of our starting directory variable.
+    __start_dir = start_dir
+
+    # Flag to indicate work was done.
+    __flag = False
+
+    try:
+        # Check to see if the mp3gain utility is installed.
+        if os.system('mp3gain -v') is not 0:
+            raise ExecError()
+        # Check to make sure we're working in a real directory.
+        if os.path.isdir(__start_dir) is False:
+            raise DirError(__start_dir)
+
+        # Iterate filesystem structure, checking each
+        # list of files contained in each directory
+        # for anything that resembles an MP3 file.
+        for __basedir, __pathnames, __files in os.walk(__start_dir):
+            for __file_ in __files:
+                try:
+                    # Better safe than sorry: let's verify
+                    # is a real directory (it should be)
+                    if os.path.isdir(__basedir) is False:
+                        raise DirError(__basedir)
+                    # Call mp3gain when we hit a
+                    # directory containing MP3s.
+                    if re.match('^.*mp3$', __file_) is not None:
+                        mp3gain(__basedir)
+                        # Raise our flag
+                        __flag = True
+                        # Keep walking...
+                        continue
+                # If we looked at a non-existent directory
+                # (deleted while running?), just go on to
+                # the next one on our walk
+                except DirError:
+                    pass
+                # End of inner isdir() try...except
+            # End of __files loop
+        # End of os.walk() loop
+    # Quit on DirError not caught inside for loop.
+    # Initial directory doesn't exist, so there's nothing to do.
+    except (DirError, ExecError):
+        return 1
+    # Unexpected error; let's find out what it is.
+    except:
+        print '\nSomething went horribly wrong on our walk!'
+        raise
+    # Made it out of the for loop with no additional errors.
+    else:
+        # Print a message indicating whether 
+        # or not files had been processed.
+        if __flag is True:
+            print '\nFinished processing files!'
+        else:
+            print '\nNo files to process!'
+    # Generic completion block for messages like
+    # '...done!' after try block has finished.
+    # (currently empty)
+    finally:
+        pass
+    # End of outer isdir() try...except block
+# End of walk() function
+
+
+# mp3gain is where we do our actual work.
+# TODO: Add messages for progress on a directory.
+def mp3gain(directory):
+    """Attach IDv3 ReplayGain tags for the MP3 files in """ \
+            """the specified directory.
+
+    Example: mp3gain.mp3gain('/path/to/Music/Artist/Album')
+    """
+
+    try:
+        # Check to make sure we're working in a real directory.
+        if os.path.isdir(directory) is False:
+            raise DirError(directory)
+
+        # Create a subprocess, and call the command
+        # inside of a shell. Command will: Calculate
+        # ReplayGain if tag does not exist, and insert
+        # it into music file as an IDv3 tag (and update
+        # any APEv2 ReplayGain tags to IDv3 tags),
+        # preserving original files' ctime, etc.
+        # TODO: Really want to see if this changes files
+        proc = subprocess.call(['/usr/bin/mp3gain -s i -p ',
+            directory, '/*.mp3'], shell=True)
+        # If /usr/bin/mp3gain returned something other
+        # than a 0, something went wrong with the process.
+        # TODO: Currently, mp3gain seems to _always_ 
+        #       return a zero on exit; I need to use 
+        #       something along the lines of Popen
+        #       and parse the output to display any
+        #       meaningful messages to the user.
+        if proc is not 0:
+            raise ProcError(directory)
+        print 'Finished with:', directory
+    # Raised when the subprocess.call() does not execute
+    # successfully (i.e. directory does not contain any MP3s).
+    # Since this can be called by itself, let's also
+    # raise directory errors right here, too.
+    except (ProcError, DirError):
+        # Return a non-zero result to indicate failure.
+        return 1
+    # Unexpected error, raise it for details.
+    except:
+        print '\nSomething went horribly wrong processing our files!'
+        raise
+    # End of isdir()|call() try...except block
+# End of mp3gain() function
+
+# End of module logic
+
+
+# Begin customized module Exceptions.
+# TODO: Clean this up to better conform to standard Excetions.
+#       Also, fix try...excepts to match same conventions.
+class Error(Exception):
+    """Base class for exceptions caused by methods in """ \
+            """the mp3gain module."""
+    pass
+
+
+class DirError(Error):
+    """Exception raised for errors relating to directory """ \
+            """existence.
+
+    Attributes:
+        dir_ -- directory which caused the error
+    """
+
+    def __init__(self, dir_):
+        self.dir_ = dir_
+        print '\nException: DirError:'
+        print 'Directory does not exist:', self.dir_
+
+    def __str__(self):
+        return repr(self.dir_)
+
+
+class ProcError(Error):
+    """Exception raised while attempting to process files """ \
+            """with the mp3gain utility.
+
+    Attributes:
+        dir_ -- directory in which the error occured
+    """
+
+    def __init__(self, dir_):
+        self.dir_ = dir_
+        print '\nException: ProcError:'
+        print 'There was an error while processing:'
+        print self.dir_
+
+    def __str__(self):
+        return repr(self.dir_)
+    
+
+class ExecError(Error):
+    """Exception raised when mp3gain utility is not installed on system.
+
+    Attributes:
+    """
+
+    def __init__(self):
+        print '\nException: ExecError:'
+        print 'The mp3gain utility is not installed on this system.'
+
+# End of customized module Exceptions.
+
+# What do we want to import using 'from mp3gain import *'
+__all__ = ['walk', 'mp3gain']
+
+# If we were called from command line...
+if __name__ == "__main__":
+    import sys
+    import os
+
+    # TODO: Put in some usage message on bad arg list.
+    # User can get to work right away number of arguments.
+    if len(sys.argv) == 2:
+        walk(sys.argv[1])
+    # Otherwise, start walking from current working directory.
+    else:
+        walk(os.getcwd())
+
+# vim: set ts=4 sts=4 sw=4:
---
+++ vim_undo.py
@@ -0,0 +1,197 @@
+#!/usr/bin/env python
+
+import os
+
+
+"""Detects unusable undofiles in Vim's undodir; for example""" \
+        """user has moved or deleted previously edited files."""
+
+
+def prune(__start_dir=os.path.join(os.getenv('HOME'), '.vim')):
+
+    """Detects unusable undofiles in user's Vim undodir.
+
+    >>> os.system('dd if=/dev/zero of=' + os.getenv('HOME') + """ \
+            """'/.vim/undo/test.vim bs=1024 count=1')
+    0
+    >>> os.system('dd if=/dev/zero of=' + os.getenv('HOME') + """ \
+            """'/.vim/view/test.py bs=1024 count=2')
+    0
+    >>> prune()
+    Cleared 3kb of disc space.
+    ...Done!
+    >>> prune('/foo/bar')
+    '/foo/bar' is not a real directory.
+    ...Done!
+    """
+
+    # Outer try block
+    try:
+        # Check to see if provided or default directory exists.
+        if not os.path.isdir(__start_dir):
+            # Throw custom exception if it does not.
+            raise DirError(__start_dir)
+
+        # Initialize __total_size to store the amount of disk space
+        # saved by deleting unusable vimfiles.
+        __total_size = 0
+
+        # Traverse the file system starting with provided or default
+        # directory, looking for unusable undo- and viewfiles.
+        for __basedir, __pathnames, __files in os.walk(__start_dir):
+
+            # We're only concerned with files located in undo- and viewdir.
+            if os.path.basename(__basedir) in ('undo', 'view'):
+
+                # Check each file in current __basedir for
+                # references to non-existant vimfiles.
+                for __file in __files:
+
+                    # Undofiles are stored in the form '%path%to%file.ext'
+                    # and can be directly translated to a form that Python
+                    # can check by replacing all occurences of '%' with
+                    # os.sep, f.e. '/' on POSIX systems (NOTE: this test has
+                    # not  been verified to work on Windows systems, and
+                    # caution is advised as the results of this function
+                    # may be irreversible).
+                    if os.path.isfile(__file.replace('%', os.sep)) is True:
+                        # __file references an existing file,
+                        # so skip to the next one.
+                        continue
+
+                    # Viewfiles are a bit more complicated, but not by much.
+                    # These files are stored in the form of
+                    # '~=+path=+to=+file.ext=' so are first task is to replace
+                    # those '=+' with the appropriate separator for the OS,
+                    # followed by stripping the trailing '=' from the end of
+                    # the filename. Because os.path.isfile() cannot into user
+                    # home directories, we'll expand that to the real path here
+                    # for the sake of later simplicity (NOTE: Again, this has
+                    # not been tested to work as expected on non-POSIX systems,
+                    # and caution is advised).
+                    elif os.path.isfile(os.path.expanduser(__file.replace('=+',
+                        os.sep).strip('='))) is True:
+                        # __file references an existing file,
+                        # so skip to the next one.
+                        continue
+
+                    # __file does not reference an existing file,
+                    # so let's try deleting it.
+                    else:
+
+                        # Try block for deleting the current __file.
+                        try:
+
+                            # Append the current filename to the current
+                            # __basedir, using the os.sep, to create an
+                            # absolute path to the __file,
+                            # f.e. '/home/user/.vim/undo/%path%to%file.ext'
+                            __absolute_file = os.path.join(__basedir, __file)
+
+                            # If the file was already removed by something
+                            # else, raise a FileError with the path.
+                            if not os.path.isfile(__absolute_file):
+                                raise FileError(__absolute_file)
+
+                            # Add the size of the file to be deleted to the
+                            # size of the files that have already been removed.
+                            __total_size += os.path.getsize(__absolute_file)
+
+                            # Delete the vimfile from the filesystem.
+                            os.remove(__absolute_file)
+
+                        # Catch the FileError, but don't do anything with it.
+                        except FileError:
+                            pass
+
+                        # End of try block for deleting the current __file.
+                    # End of __file checking if-elif-else block.
+                # End of __files for loop.
+            # End of 'undo'/'view' if block.
+        # End of os.walk() for loop.
+
+    # In the case of the __start_dir being invalid...
+    except DirError as e:
+        # ...catch fatal custom exception, and report it to the user.
+        print "{} is not a real directory.".format(e)
+
+    # Do nothing with any other uncaught exceptions.
+    except:
+        pass
+
+    # If outer try block executed with no exceptions...
+    else:
+        # ...report the amount of disk space freed in kilobytes.
+        print "Cleared {}kb of disc space.".format(__total_size / 1024)
+
+    # At end of execution, print a message stating we're done.
+    finally:
+        print "...Done!"
+
+    # End of outer try block.
+
+
+# Define doctest test here.
+# def _test():
+    # import doctest
+    # doctest.testmod(verbose=True)
+
+
+# Begin customized module Exceptions.
+# TODO: Clean this up to better conform to standard Excetions.
+#       Also, fix try...excepts to match same conventions.
+class Error(Exception):
+    """Base class for exceptions caused by methods in """ \
+            """the vim-undo module."""
+    pass
+
+
+class DirError(Error):
+    """Exception raised for errors relating to directory """ \
+            """existence.
+
+    Attributes:
+        dir_ -- directory which caused the error
+    """
+
+    def __init__(self, dir_):
+        self.dir_ = dir_
+        # print '\nException: DirError:'
+        # print 'Directory does not exist:', self.dir_
+
+    def __str__(self):
+        return repr(self.dir_)
+
+
+class FileError(Error):
+    """Exception raised for errors relating to file """ \
+            """existence.
+
+    Attributes:
+        file_ -- directory which caused the error
+    """
+
+    def __init__(self, file_):
+        self.file_ = file_
+        # print '\nException: FileError:'
+        # print 'File does not exist:', self.file_
+
+    def __str__(self):
+        return repr(self.file_)
+
+
+__all__ = ['prune']
+
+# If we were called from command line...
+if __name__ == "__main__":
+    import sys
+
+    # TODO: Put in some usage message on bad arg list.
+    # User can get to work right away number of arguments.
+    if len(sys.argv) == 2:
+        prune(sys.argv[1])
+    # Otherwise, start walking from current working directory.
+    else:
+        prune()
+
+# vim: set ts=4 sts=4 sw=4:
