afbb8f41111b97cccdde27a21c4aca459bf1cfa7
dtsteinm <dtsteinm@gmail.com>
2013-03-17 02:03:29 -0400

    Replaced non-ASCII &copy; with '(C)'.

--- mp3gain.py
+++ mp3gain.py
@@ -5,7 +5,7 @@ import subprocess
 import re
 
 # mp3gain Python module
-# Copyright © 2013 Dylan Steinmetz <dtsteinm@gmail.com>
+# Copyright (C) 2013 Dylan Steinmetz <dtsteinm@gmail.com>
 # This work is free. You can redistribute it and/or modify it under the
 # terms of the Do What The Fuck You Want To Public License, Version 2,
 # as published by Sam Hocevar. See the COPYING file for more details.
--- vim_undo.py
+++ vim_undo.py
@@ -4,7 +4,7 @@ import os
 
 
 # vim_undo Python module
-# Copyright © 2013 Dylan Steinmetz <dtsteinm@gmail.com>
+# Copyright (C) 2013 Dylan Steinmetz <dtsteinm@gmail.com>
 # This work is free. You can redistribute it and/or modify it under the
 # terms of the Do What The Fuck You Want To Public License, Version 2,
 # as published by Sam Hocevar. See the COPYING file for more details.
