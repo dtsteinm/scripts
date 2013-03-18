a56e369f687e079c15c75e83d17a27312db21945
dtsteinm <dtsteinm@gmail.com>
2013-03-18 17:39:17 -0400

    Filled out skeleton for media_list.py

--- media_list.py
+++ media_list.py
@@ -12,18 +12,99 @@ import os
 
 
 """Explanation here."""
+# TODO: Consider writing this OO with class
 
-def tryconvert(episode):
-    """Explanation here."""
+
+def make_playlist(start_dir=os.getcwd(), playlist_type='pls'):
+    """Create media playlist(s) for specified directory and playlist"""\
+            """type.
+    
+    Attributes:
+        start_dir -- directory from which to start; if None, prompt user?
+        playlist_type -- type of playlist to create; either 'pls' or 'm3u'
+    """
+
+def find_files(dir_=os.getcwd()):
+    """Finds directories that contain media files from a given starting"""\
+            """point, and tracks the lists.
+
+    Attributes:
+        dir_ -- directory in which to search for media files; cwd by default
+    """
+    pass
+
+
+def mkpls(file_list):
+    """Creates a PLS format playlist file from a list of filenames."""\
+
+    """Attributes:
+        file_list -- sorted list of files
+    """
+    pass
+
+
+def mkm3u(file_list):
+    """Creates a M3U format playlist file from a list of filenames."""\
+
+    """Attributes:
+        file_list -- sorted list of files
+    """
+    pass
+
+
+# TODO: The looping sorting should probably be split up so that
+#       we get a list for each directory that contains media files,
+#       rather the the list from the final directory.
+# FIXME: We also need to return the basedir associated with each list
+#        in order to get absolute paths in the mkpls and mkm3u functions.
+def sort_files(current_dir):
+    """Returns sorted list of files in passed directory.
+
+    Attributes:
+        current_dir -- directory from which we should look for files
+    """
+    for basedir, pathnames, files in os.walk(current_dir):
+        sorted_list = sorted(files, key=lambda number: tryconvert(number))
+    return sorted_list
+
+
+def tryconvert(filename):
+    """Extracts a sequence number from a complex filename, ignoring"""\
+            """CRC checksums, and returns result as an integer.
+
+    Attributes:
+        filename -- Filename from which sequence is to be extracted
+
+    Examples:
+    >>> tryconvert('Testfile_No._22_[CRC32CRC].ext')
+    22
+    """
+
+    # If filename does not contain a non-CRC number, re.findall(...)[0]
+    # will throw an IndexError exception for the empty list produced.
+    # TODO: Optional user-specified sorting, maybe?
     try:
-        return int(re.findall('([0-9]+)',re.sub('([0-9]|[A-Z]){8}','X',episode[::-1], 1)[::-1])[0])
+
+        # Finds and returns (as an integer) a sequence number from
+        # a complex filename. NOTE: May not work with all formats.
+        # Explanation:
+        # - filename[::-1]: reverse the string; essentially starts
+        #                     re.sub from last character
+        # - re.sub(..., 1)[::-1]: replace CRC32s with a single 'X'
+        #                         and un-reverse string
+        # - int.findall(...)[0]: find all occurences of one or more
+        #                        numbers, and reference only the first
+        # - int(...): cast string from re.findall() to int; key in
+        #             sorted() only takes an integer?
+        return int(re.findall('([0-9]+)', re.sub('[0-9A-Fa-f]{8}',
+            'X', filename[::-1], 1)[::-1])[0])
+
+    # If the filename did not contain a sequence number, return
+    # something unreasonably large to place it at the end of the
+    # sorted list.
     except IndexError:
         return 9999
 
-def list(files):
-    """Explanation here."""
-    for base, paths, files in os.walk(os.getcwd()):
-        sorted(files, key=lambda number: tryconvert(number))
 
 # Begin customized module Exceptions.
 # TODO: Clean this up to better conform to standard Excetions.
