616e9c68d2278ad0a248748e4023bd2fe91e8670
dtsteinm <dtsteinm@gmail.com>
2013-04-08 00:18:30 -0400

    PEP8 corrections.

--- punny.py
+++ punny.py
@@ -11,37 +11,40 @@ import Levenshtein
 
 __all__ = ['PunGenerator']
 __author__ = 'Dylan Steinmetz <dtsteinm@gmail.com>'
-__version__ = '0.1'
+__version__ = '0.2'
 __license__ = 'WTFPL'
+
 
 class PunGenerator:
     '''Generate the puns.'''
 
     # TODO: Add other pun lists
-    # TODO: Use dictionaries with preferred replacements:
-    #       f.e. 'mouth' -> 'beak'
-    squid = ['ink','fin','gill','beak','kraken','mollusk']
+    # TODO: Add automatic words for other puns
+    squid = {'ink': '', 'fin': '', 'gill': '', 'beak': ['mouth', 'face'],
+            'kraken': '', 'mollusk': ''}
 
     # TODO: Allow choosing pun list on object creation
     def __init__(self):
-        # Sort the list of puns by length; might come in handy 
-        # if we want to try to optimize the comparisons.
-        self.puns = sorted(self.squid, key=lambda pun: len(pun))
+        self.puns = self.squid
 
     def select_pun(self, string):
         '''Choose the best available pun.'''
-        
+
         # Store the currently best pun option, and it's Levenshtein
         # ratio (0-1) in a tuple to compare it to later options.
-        best_pun = ('',0)
+        best_pun = ('', 0)
 
         # Compare each pun option; better puns replace earlier options.
         for pun in self.puns:
+            # Check to see if there is a defined replacement in the
+            # pun dictionary.
+            if string in self.puns[pun]:
+                return pun, 1
             current_ratio = Levenshtein.ratio(pun, string)
             if current_ratio > best_pun[1]:
                 best_pun = pun, current_ratio
 
-        # Return both the pun itself, and the ratio so that 
+        # Return both the pun itself, and the ratio so that
         # generate_pun() can find the best pun for a phrase.
         return best_pun
 
@@ -52,7 +55,7 @@ class PunGenerator:
 
         # Store the currently best pun option, and it's Levenshtein
         # ratio (0-1) in a tuple to compare it to later options.
-        best_pun = ('',0)
+        best_pun = ('', 0)
 
         # Find the best word to use and it's corresponding best pun.
         # TODO: Allow for sub-string replacements, f.e. 'ink-vade'
@@ -68,6 +71,8 @@ class PunGenerator:
     def add_pun(self, string):
         '''Add a new pun to the list of available puns.'''
 
-        # Add a new pun to the list, and re-sort it by length.
-        self.puns.append(string)
-        self.puns = sorted(self.puns, key=lambda pun: len(pun))
+        # Add a new pun to the dictionay.
+        # TODO: Add corresponding words as well
+        self.puns[string] = ''
+
+# TODO: Add command line stuff down here
